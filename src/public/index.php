332393eff3f68cbe187e0f3e4dd8545f1cc76547
Mergn89 <mergen.olegov@yandex.com>
2024-12-07 12:03:03 +0800

    добавляет автозагрузку

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,46 @@
 <?php
-require_once './../Core/App.php';
-require_once './../Controller/UserController.php';
-require_once './../Controller/ProductController.php';
-require_once './../Controller/CartController.php';
-require_once './../Controller/OrderController.php';
+//require_once './../Core/App.php';
+//require_once './../Controller/UserController.php';
+//require_once './../Controller/ProductController.php';
+//require_once './../Controller/CartController.php';
+//require_once './../Controller/OrderController.php';
 
+$autoloadCore = function(string $className) {
+    $path = "./../Core/$className.php";
+    if(file_exists($path)){
+        require_once $path;
+        return true;
+    }
+    return false;
+};
+
+$autoloadController = function(string $className) {
+    require_once "./../Controller/$className.php";
+    $path = "./../Core/$className.php";
+    if(file_exists($path)){
+        require_once $path;
+        return true;
+    }
+    return false;
+};
+
+$autoloadModel = function(string $className) {
+    $path = "./../Model/$className.php";
+    if(file_exists($path)){
+        require_once $path;
+        return true;
+    }
+    return false;
+};
+
+spl_autoload_register($autoloadCore);
+spl_autoload_register($autoloadController);
+spl_autoload_register($autoloadModel);
 
 $app = new App();
 $app->run();
+
+
+
+
 
